f6ab422881289e2ac7cb298288cb7500bd14c295
kouts <koutsaftakis@gmail.com>
2020-09-09 16:41:54 +0300

    Added SCSS support in components and purgecss in build

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  lintOnSave: 'warning',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/scss/variables.scss";
+        `
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'ProjectName';
+      args[0].meta = { description: 'A single page application created using Vue.js 3' };
+      return args;
+    });
+  }
+};
